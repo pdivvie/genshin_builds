05a1160274a319818b6840b777f6d5211de39742
pdivvie <pdivvie@outlook.com>
2021-02-15 13:38:22 +0200

    Initial commit

---
+++ app/src/main/java/com/devilliersdesigns/Albedo.kt
@@ -0,0 +1,31 @@
+package com.devilliersdesigns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devilliersdesigns.fragments.adapters.ViewPagerAdapter
+import com.devilliersdesigns.fragments.al_build_1_fragment
+import com.devilliersdesigns.fragments.al_build_2_fragment
+import kotlinx.android.synthetic.main.activity_albedo.*
+
+class Albedo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_albedo)
+
+        val actionBar = supportActionBar
+
+        "Albedo".also { actionBar!!.title = it }
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setUpTabs()
+    }
+
+    private fun setUpTabs() {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(al_build_1_fragment(), "Build 1")
+        adapter.addFragment(al_build_2_fragment(), "Build 2")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+    }
+}
